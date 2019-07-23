0f4cd377239a79bb9ed75ce4f174b5e09ef30192
Kenny <kcheng4@villanova.edu>
2019-07-23 18:33:07 -0400

    Adding documentation

--- controllers/getStudentAttendance.js
+++ controllers/getStudentAttendance.js
@@ -1,5 +1,9 @@
+//Student variable takes in data from the json file provided
 const students = require('./students.json');
 
+//getStudentAttendance function takes in a number and the function filters
+//out students that are above said number and provides an array of 
+//students below the threshold
 const getStudentAttendance = (attendancePercent)=>{
     return students.filter((el)=>{
         return el.attendancePercentage<attendancePercent;
--- server.js
+++ server.js
@@ -3,17 +3,21 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const {getStudentAttendance} = require('./controllers/getStudentAttendance');
 
+//Express app is used for API creation
 const app = express();
+//port is either 3000 for development environment or a specified one for prod
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
 
+//GET endpoint for fetching a list of students below a certain attendance threshold
 app.get('/:attendancepercent',(req,res)=>{
     const attendancePercent = req.params.attendancepercent;
     res.send(getStudentAttendance(attendancePercent));
 });
 
+//Server start
 app.listen(port,()=>{
     console.log(`App is running on port ${port}`);
 })
